63b4ac2a46d3be4348072b3f515b3537545fe465
Xinling Jing <jingxinling1024@163.com>
2021-11-12 18:10:53 +0800

    fix: 修复网速显示文本过长换行溢出问题

--- modules/network.lua
+++ modules/network.lua
@@ -52,7 +52,7 @@ function scan()
         obj.last_down = obj.current_down
         obj.last_up = obj.current_up
 
-        local canvas = hs.canvas.new{x = 0, y = 0, h = 24, w = 60}
+        local canvas = hs.canvas.new{x = 0, y = 0, h = 24, w = 70}
         canvas[1] = {type = 'text', text = obj.display_text}
         menubar:setIcon(canvas:imageFromCanvas())
         canvas:delete()
