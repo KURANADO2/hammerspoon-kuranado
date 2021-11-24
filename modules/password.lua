ed98b19310c23facf11f913282a5786fbeb48f11
Xinling Jing <jingxinling1024@163.com>
2021-11-24 15:13:06 +0800

    feat: 更改密码粘贴快捷键为 Ctrl + Cmd + V
    之前快捷键为 Cmd + Option + V 和系统剪切快捷键冲突

--- modules/password.lua
+++ modules/password.lua
@@ -1,4 +1,4 @@
 -- 将剪贴板中的字符以按键事件发送（解决某些网站禁止粘贴密码问题）
-hs.hotkey.bind({"cmd", "alt"}, "v", "Password paste", function()
+hs.hotkey.bind({"ctrl", "cmd"}, "v", "Password Paste", function()
     hs.eventtap.keyStrokes(hs.pasteboard.getContents())
 end)
