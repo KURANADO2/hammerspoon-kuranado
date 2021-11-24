f66c7a19df982365ccc4556fed3ee22f860b62e5
Xinling Jing <jingxinling1024@163.com>
2021-11-24 15:14:55 +0800

    style: 使用 canvas 绘制代替字符 '|'  作为分隔线

--- modules/hotkey.lua
+++ modules/hotkey.lua
@@ -4,8 +4,11 @@ require 'modules.base'
 
 local screen = hs.window.focusedWindow():screen():frame()
 
-local COORIDNATE_X = screen.w / 2;
-local COORIDNATE_Y = screen.h / 2;
+local COORIDNATE_X = screen.w / 2
+local COORIDNATE_Y = screen.h / 2
+
+-- 快捷键总数
+local num = 0
 
 -- 创建 Canvas
 local canvas = hs.canvas.new({x = 0, y = 0, w = 0, h = 0})
@@ -25,13 +28,12 @@ function formatText()
     local hotkeys = hs.hotkey.getHotkeys()
     local renderText = {}
 
-    -- TODO-JING 提示标题单独渲染居中展示
-    local title = '\n Shortcut List：\n\n'
     -- 每行最多 40 个字符
     local MAX_LEN = 40
 
     -- 文本定长
     for k, v in ipairs(hotkeys) do
+        num = num + 1
         local msg = v.msg
         local len = utf8len(msg)
         -- 超过最大长度，截断多余部分，截断的部分作为新的一行
@@ -52,29 +54,20 @@ end
 function drawText(renderText)
     -- 每列最多 20 行
     local MAX_LINE_NUM = 20
-    local font = {
-        name = "Monaco",
-        size = 14
-    }
-    local color = {hex = '#0096FA'}
     local w = 0
     local h = 0
+    -- 文本距离分割线的距离
+    local SEPRATOR_W = 5;
+
     -- 每一列需要显示的文本
     local column = ''
-    -- 元素数目
-    local num = 0
     for k, v in ipairs(renderText) do
-        num = num + 1
         local line = v.line
-        -- TODO-JING 分割线用 Canvas 绘制搞定
-        column = column .. line .. ' | \n'
+        column = column .. line .. '  \n'
         -- k mod MAX_LINE_NUM
         if math.fmod(k, MAX_LINE_NUM) == 0 then
-            local titleText = hs.styledtext.new(column, {
-                font = font,
-                color = color
-            })
-            local size = canvas:minimumTextSize(titleText)
+            local itemText = styleText(column)
+            local size = canvas:minimumTextSize(itemText)
             -- 多 text size w 累加
             w = w + size.w
             if k == MAX_LINE_NUM then
@@ -82,29 +75,48 @@ function drawText(renderText)
             end
             canvas:appendElements({
                 type = 'text',
-                text = titleText,
-                frame = {x = (k / MAX_LINE_NUM - 1) * size.w, y = 0, w = size.w, h = size.h}
+                text = itemText,
+                frame = {x = (k / MAX_LINE_NUM - 1) * size.w + SEPRATOR_W, y = 0, w = size.w + SEPRATOR_W, h = size.h}
+            })
+            canvas:appendElements({ 
+                type = "segments", 
+                closed = false, 
+                -- strokeColor = { blue = 1 }, 
+                strokeColor = { hex = '#0096FA' },
+                action = "stroke",
+                strokeWidth = 2,
+                coordinates = {{ x = (k / MAX_LINE_NUM) * size.w - SEPRATOR_W, y = 0 }, {x = (k / MAX_LINE_NUM) * size.w - SEPRATOR_W, y = h}},
             })
             column = ''
         end
     end
     if column ~= nil then
-        local titleText = hs.styledtext.new(column, {
-            font = font,
-            color = color
-        })
-        local size = canvas:minimumTextSize(titleText)
+        local itemText = styleText(column)
+        local size = canvas:minimumTextSize(itemText)
         w = w + size.w
         canvas:appendElements({
             type = 'text',
-            text = titleText,
-            frame = {x = math.ceil(num / MAX_LINE_NUM - 1) * size.w, y = 0, w = size.w, h = size.h}
+            text = itemText,
+            frame = {x = math.ceil(num / MAX_LINE_NUM - 1) * size.w + SEPRATOR_W, y = 0, w = size.w + SEPRATOR_W, h = size.h}
         })
         column = nil
     end
     
     -- 居中显示
     canvas:frame({x = COORIDNATE_X - w / 2, y = COORIDNATE_Y - h / 2, w = w, h = h})
+end
+
+function styleText(text)
+    return hs.styledtext.new(text, {
+        font = {
+            name = "Monaco",
+            size = 14
+        },
+        color = {hex = '#0096FA'},
+        paragraphStyle = {
+            lineSpacing = 5
+        }
+    })
 end
 
 -- 默认不显示
