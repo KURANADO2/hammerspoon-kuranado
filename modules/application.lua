93aed131e33258f27b49fe260d4222e000b6c9ef
Xinling Jing <jingxinling1024@163.com>
2022-07-30 20:02:12 +0800

    refactor: 重构应用切换相关逻辑
    要求：
    - 快捷键切换应用
    - 保存鼠标在每一个窗口下的位置，下次切换到该窗口时，自动移动鼠标到原来的位置
    
    Fixes #15

--- modules/application.lua
+++ modules/application.lua
@@ -8,60 +8,35 @@ hs.fnutils.each(applications, function(item)
     end)
 end)
 
--- 鼠标位置
-mousePositions = {}
+-- 存储鼠标位置
+local mousePositions = {}
 
 function toggleAppByBundleId(appBundleID)
-    -- 获取当前最靠前的应用,保存鼠标位置
-    local frontMostApp = hs.application.frontmostApplication()
-    -- 当前无最靠前的应用窗口或最靠前的应用的窗口
-    if frontMostApp ~= nil and frontMostApp:mainWindow() ~= nil then
-        mousePositions[frontMostApp:mainWindow():id()] = hs.mouse.absolutePosition
+
+    local previousFocusedWindow = hs.window.focusedWindow()
+    mousePositions[previousFocusedWindow:id()] = hs.mouse.absolutePosition()
+
+    hs.application.launchOrFocusByBundleID(appBundleID)
+
+    -- 获取 application 对象
+    local applications = hs.application.applicationsForBundleID(appBundleID)
+    local application = nil
+    for k, v in ipairs(applications) do
+        application = v
     end
 
-    -- 两者重复时,寻找下一个该窗口
-    if frontMostApp:bundleID() == appBundleID then
-        local wf = hs.window.filter.new{frontMostApp:name()}
-        local locT = wf:getWindows({hs.window.filter.sortByFocusedLast})
-        if locT and #locT > 1 then
-            local windowId = frontMostApp:mainWindow():id()
-            for _, value in pairs(locT) do
-                if value:id() ~= windowId then
-                    value:focus()
-                end
-            end
-        else
-            frontMostApp:hide()
-        end
+    local currentFocusedWindow = application:focusedWindow()
+    if mousePositions[currentFocusedWindow:id()] ~= nil then
+        hs.mouse.absolutePosition(mousePositions[currentFocusedWindow:id()])
     else
-        -- 不存在窗口时,启动app
-        local launchResult = hs.application.launchOrFocusByBundleID(appBundleID)
-        if not launchResult then
-            return
-        end
+        setMouseToCenter(currentFocusedWindow)
     end
-    -- 调整鼠标位置
-    frontMostApp = hs.application.applicationsForBundleID(appBundleID)[1]
-    local point = mousePositions[appBundleID]
-    if point then
-      hs.mouse.setAbsolutePosition(point)
-      local currentSc = hs.mouse.getCurrentScreen()
-      local tempSc = frontMostApp:mainWindow():screen()
-      if currentSc ~= tempSc then
-          setMouseToCenter(frontMostApp)
-      end
-    -- 找不到则转移到该屏幕中间
-    else
-        setMouseToCenter(frontMostApp)
-    end
+
+    local windows = application:visibleWindows()
 end
 
-function setMouseToCenter(frontMostApp)
-    local mainWindow = frontMostApp:mainWindow()
-    if not mainWindow then
-        return
-    end
-    local mainFrame = mainWindow:frame()
-    local mainPoint = hs.geometry.point(mainFrame.x + mainFrame.w /2, mainFrame.y + mainFrame.h /2)
-    hs.mouse.absolutePosition(mainPoint)
+function setMouseToCenter(foucusedWindow)
+    local frame = foucusedWindow:frame()
+    local centerPosition = hs.geometry.point(frame.x + frame.w / 2, frame.y + frame.h / 2)
+    hs.mouse.absolutePosition(centerPosition)
 end
